892f9ca67acfc395b01827c1d7861390fc263827
Natalie Halawani <hala1224@gmail.com>
2019-03-05 04:17:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -27,6 +27,18 @@ class App < Sinatra::Base
   end
 
   get "/:operation/:number1/:number2" do 
-    
+    @operation = params[:operation]
+    @number1 = params[:number1].to_i
+    @number2 = params [:number2].to_i
+    if @operation == "add"
+      "#{@number1 + @number2}"
+    elsif @operation == "subtract"
+      "#{@number1 - @number2}"
+    elsif @operation == "multiply"
+      "#{@number1 * @number2}"
+    elsif @operation == "divide"
+      "#{@number1 / @number2}"
+    end
+  end
 
 end
