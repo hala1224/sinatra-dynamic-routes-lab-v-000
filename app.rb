cc73b79e30e79c5a5a543ab26d6c4def07a3b9ca
Natalie Halawani <hala1224@gmail.com>
2019-03-05 04:08:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,6 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+ get "/reversename/:name" do
+    @user_name = params[:name]
+    "#{@user_name} #{@user_name.reverse}!!"
+  end
+
+  get "/square/:number" do
+    @num = params[:number]
+    "#{@num.to_i ** 2}"
+  end
 
 
 
